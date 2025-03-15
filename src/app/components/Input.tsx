96b2fea174e73e53268cdd874bb5a7914b449d4d
Sebastian Wojciechowski <se.wojciechowski6@gmail.com>
2025-03-15 07:43:38 +0100

    Sign should be togglable

--- src/app/components/Input.tsx
+++ src/app/components/Input.tsx
@@ -1,11 +1,21 @@
+"use client";
+import { useState } from "react";
+
 export default function Input() {
-  const sign = "-";
+  const [sign, setSign] = useState("-");
+
+  const toggleSign = () => {
+    setSign((prevSign) => (prevSign === "-" ? "+" : "-"));
+  };
 
   return (
     <div className="grid grid-cols-5 border border-gray-300">
-      <div className="border-r border-r-gray-300 p-2 text-center content-center">
+      <button
+        onClick={toggleSign}
+        className="border-r border-r-gray-300 p-2 text-center content-center hover:bg-gray-100"
+      >
         {sign}
-      </div>
+      </button>
       <input className="col-span-4 p-2" type="text" />
       <textarea className="border-t border-t-gray-300 col-span-5 row-span-2 resize-none p-2" />
     </div>
