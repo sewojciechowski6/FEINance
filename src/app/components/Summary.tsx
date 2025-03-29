dc10a0aba0ae45dbb6644aaa199c1efb0a76a799
Sebastian Wojciechowski <se.wojciechowski6@gmail.com>
2025-03-29 06:14:29 +0100

    Color summary

--- src/app/components/Summary.tsx
+++ src/app/components/Summary.tsx
@@ -5,13 +5,15 @@ export default async function Summary() {
 
   return (
     <div className="border border-gray-300 grid grid-cols-2 grid-rows-2">
-      <div className="border-b border-b-gray-300 col-span-2 p-2 text-center content-center">
+      <div className="border-b border-b-gray-300 col-span-2 p-2 text-center content-center bg-green-100">
         {totalIncome}
       </div>
-      <div className="border-r border-r-gray-300 p-2 text-center content-center">
+      <div className="border-r border-r-gray-300 p-2 text-center content-center bg-red-100">
         {totalExpense}
       </div>
-      <div className="p-2 text-center content-center">{balance}</div>
+      <div className="p-2 text-center content-center bg-orange-100">
+        {balance}
+      </div>
     </div>
   );
 }
